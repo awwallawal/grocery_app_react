b0b59fd80f14ef464076984e9973e020a8839ebd
Awwal <awwallawal@gmail.com>
2023-05-11 16:22:14 +0100

    Working on the checkboxes to change their state using both the Imperative JavaScript Way and the Declarative React Way

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -21,19 +21,57 @@ function MainContent() {
     },
     ]);
 
+    const handleChecked = (id) => {
+      // alert('Awwal')
+      // alert(`You have cliked Item ${id}`)
+      // We want this handler to do is change the checked boolean from true to false (remember the dice game)
+      // There are ways to achieve this either through the declarative React way or imperative JavaScript way
+
+      /*Imperative Vanilla JavaScript Way
+      const copyItemList = [...items];
+      for (let objects of copyItemList){
+        if(objects.id === id){
+          objects.checked = !objects.checked
+        }
+      }
+      setItems(copyItemList);
+      */
+      // React Declarative Way
+      const ListItems = items.map((item)=>{
+          if (item.id === id){
+            item.checked = ! item.checked
+          }
+          return item
+      })
+      setItems(ListItems);
+    }
+
   return (
     <main>
       <ul>
         {items.map((item) => {
           return (
-            <li className='item' key={item.id}>
-              <input type="checkbox" checked={item.checked} />
-              <label htmlFor="">{item.item}</label>
-              {/* <button>Delete</button>  Replaced the delete button with an icon*/}
-              <FaTrashAlt 
-                role="button"
-                tabIndex="0"
-              />
+            <li 
+            className='item' 
+            key={item.id}
+            >
+                  <input 
+                  type="checkbox" 
+                  checked={item.checked} 
+                  onClick={() =>{handleChecked(item.id)}} 
+                  />
+
+                  <label htmlFor="">
+                    {item.item}
+                  </label>
+
+                  {/* <button>Delete</button>  Replaced the delete button with an icon*/}
+
+                  <FaTrashAlt 
+                    role="button"
+                    tabIndex="0"
+                  />
+              
             </li>
           )
         })}
